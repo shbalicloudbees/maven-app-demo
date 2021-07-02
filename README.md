79db81b53305f59f231401548ac574b941d2dfd3
Sara hbali <shbali@cloudbees.com>
2021-07-02 10:41:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# maven-spring-boot-demo
+Demo with maven and spring boot 2 and java 10
+
+[![DepShield Badge](https://depshield.sonatype.org/badges/joostvdg/maven-spring-boot-demo/depshield.svg)](https://depshield.github.io)
---
+++ src/main/java/com/github/joostvdg/demo/mavenspringdemo/MavenSpringDemoApplication.java
@@ -0,0 +1,12 @@
+package com.github.joostvdg.demo.mavenspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenSpringDemoApplication.class, args);
+	}
+}
